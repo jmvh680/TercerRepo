fa156cb44fcdc0f51721a8b1e83c4de0ebde761e
jmvh680 <juan680@hotmail.com>
2025-04-07 08:00:20 -0600

    Arcivo local

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ import React from 'react'
 
 function Home() {
     const listItems = [1,2,3,4]
-/*
-
-ESTE ES EL NUEVO TEXTO
 
 */
     const addItems = () => {
         const variableA = 1;
         const variableB = 2;
         const variableC = 3;
+        const variableD = 4;
 
-        return variableA * 3 + variableB + variableC
+        return variableA * 3 + variableB + variableC * variableD
     }
     return (
         <><div>Home</div><button onclick={addItems}/></>
