1bb3a3be9d246fb4c5018f5ccb9f581e8c888add
jmvh680 <juan680@hotmail.com>
2025-04-06 22:54:56 -0600

    primera modificacion archivo js

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+    const listItems = [1,2,3,4]
+
+    const addItems = () => {
+        const variableA = 1;
+        const variableB = 2;
+        const variableC = 3;
+
+        return variableA * 3 + variableB + variableC
+    }
+    return (
+        <><div>Home</div><button onclick={addItems}/></>
+    )
+}
+
+export default Home
